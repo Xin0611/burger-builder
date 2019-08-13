7382e5dbc40616ffd67aaa7dd707bcd4556266d3
Xin Chen <xinchen921005@gmail.com>
2019-08-13 15:37:11 -0700

    added ingredients sum check

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedingredients = Object.keys(props.ingredients)
-    .map(igKey => {
+  let transformedingredients = Object.keys(props.ingredients)
+    .map( igKey => {
       return [...Array(props.ingredients[igKey])]
         .map((_, i) => {
           return <BurgerIngredient key={igKey+i} type={igKey}/>
-        })
-    })
+        } )
+    } )
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    },[] );
+  if(transformedingredients.length === 0) {
+    transformedingredients = <p>Please start adding ingredients</p>
+  }
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
