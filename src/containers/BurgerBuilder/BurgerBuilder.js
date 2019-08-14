ac76d33d0f6499f697a2c92246ad034ea9033c46
Xin Chen <xinchen921005@gmail.com>
2019-08-14 14:44:37 -0700

    added Order button, disabled when no ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,9 +19,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
   }
-
+  updatePurchaseState (ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum+el;
+      },0);
+      this.setState({purchasable: sum > 0});
+  }
   addIngredientHandler = (type) => {
     const updateCount = this.state.ingredients[type] + 1;
     const updatedIngredients = {
@@ -33,6 +43,7 @@ class BurgerBuilder extends Component {
       totalPrice: updatedPrice,
       ingredients: updatedIngredients
     })
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -49,7 +60,8 @@ class BurgerBuilder extends Component {
     this.setState({
       totalPrice: updatedPrice,
       ingredients: updatedIngredients
-    })
+    });
+    this.updatePurchaseState(updatedIngredients);
   }
   render() {
     const disabledInfo = {
@@ -67,6 +79,7 @@ class BurgerBuilder extends Component {
         ingredientRemoved={this.removeIngredientHandler}
         disabled={disabledInfo}
         price={this.state.totalPrice}
+        purchasable={this.state.purchasable}
         />
       </Aux>
     );
