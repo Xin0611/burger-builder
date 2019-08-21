bdad2ecad23aebd635b2e719dd538317496ac68b
Xin Chen <xinchen921005@gmail.com>
2019-08-20 23:22:54 -0700

    Route to ContactData Component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from '../../containers/Checkout/ContactData/ContactData';
+
 class Checkout extends Component {
   state = {
     ingredients: {
@@ -33,6 +36,7 @@ class Checkout extends Component {
           ingredients={this.state.ingredients} 
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}/>
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     );
   }
