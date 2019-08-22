d3b264a6cfe9e36db20be7cfead709bb2fa22c87
Xin Chen <xinchen921005@gmail.com>
2019-08-21 20:37:30 -0700

    displaying spinner during loading time after click order button

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 
@@ -43,18 +44,24 @@ class ContactData extends Component {
       });
   }
   render () {
+    let form = (
+      <form>
+        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+        <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+        <input className={classes.Input} type="text" name="street" placeholder="Street" />
+        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+        <Button 
+          btnType="Success"
+          clicked={this.orderHandler}>Order</Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-          <input className={classes.Input} type="text" name="street" placeholder="Street" />
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button 
-            btnType="Success"
-            clicked={this.orderHandler}>Order</Button>
-        </form>
+        {form}
       </div>
     )
   }
