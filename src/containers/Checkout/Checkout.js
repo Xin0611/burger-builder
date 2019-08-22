d63c588e0bd514b876b7b1c4188e8b2cb5d84ff8
Xin Chen <xinchen921005@gmail.com>
2019-08-21 20:40:09 -0700

    redirecting to home page after submmit order successfully

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -39,7 +39,7 @@ class Checkout extends Component {
           checkoutContinued={this.checkoutContinuedHandler}/>
         <Route 
           path={this.props.match.path + '/contact-data'} 
-          render={() => (<ContactData ingredients={this.state.ingredients} price={this.state.price}/>)} />
+          render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.price} {...props} />)} />
       </div>
     );
   }
