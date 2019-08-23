04e35f9118e7f581cb9aa699b1b71517f26f1562
Xin Chen <xinchen921005@gmail.com>
2019-08-22 23:13:24 -0700

    added validation feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -156,10 +156,12 @@ class ContactData extends Component {
       <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input 
-            key={formElement.id}
+            key={formElement.id}å
             elementType={formElement.config.elementType} 
             elementConfig={formElement.config.elementConfig} 
             value={formElement.config.value} 
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             onChange={(event) => this.inputChangedHandler(event, formElement.id)}/>
         ))}
         <Button 
