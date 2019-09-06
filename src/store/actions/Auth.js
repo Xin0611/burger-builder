93307d3e4a8ad7148c0be1cde66cd69878358145
Xin Chen <xinchen921005@gmail.com>
2019-09-05 23:45:47 -0700

    add actions in Redux for Auth

---
+++ src/store/actions/Auth.js
@@ -0,0 +1,31 @@
+import {
+  AUTH_START,
+  AUTH_SUCESS,
+  AUTH_FAIL,
+} from './actionTypes';
+
+export const authStart = () => {
+  return {
+    type: AUTH_START
+  }
+}
+
+export const authSucess = (authData) => {
+  return {
+    type: AUTH_SUCESS,
+    authData
+  }
+}
+
+export const authFail = (error) => {
+  return {
+    type: AUTH_FAIL,
+    error
+  }
+}
+
+export const auth = (email, password) => {
+  return dispatch => {
+    dispatch(authStart);
+  }
+}
